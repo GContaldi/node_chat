241aa3806e5e77267fe154f2e205f213163c82a1
Gaetano Contaldi <gaetanocontaldi000@gmail.com>
2017-04-05 14:37:01 +0200

    use localStorage in store

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,17 @@ import { createStore, applyMiddleware } from 'redux';
 import createSocketIoMiddleware from 'redux-socket.io';
 import io from 'socket.io-client';
 import reducer from './reducer';
+import { initLocalStorage, updateLocalStorage, getStateFromLocalStorage } from './localStorage';
 
 const socket = io();
 const socketIoMiddleware = createSocketIoMiddleware(socket, 'server/');
 
-const store = applyMiddleware(socketIoMiddleware)(createStore)(reducer);
+initLocalStorage();
+
+const store = applyMiddleware(socketIoMiddleware)(createStore)(reducer, getStateFromLocalStorage());
+
+store.subscribe(() => {
+  updateLocalStorage(store.getState());
+});
 
 export default store;
